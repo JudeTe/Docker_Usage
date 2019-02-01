574fc5b6f0617373a367e536c1e6d4b5a7124311
Jude <45480093+JudeTe@users.noreply.github.com>
2019-02-01 22:43:09 +0800

    Add files via upload

--- docker_detect.py
+++ docker_detect.py
@@ -4,8 +4,11 @@ from lineNotify import lineNotify
 if os.path.exists("docker_usage.txt"):
     with open("docker_usage.txt", "r", encoding="utf-8") as f:
         output = f.read().split(" ")[2]
+        IntOutput = output.split(".")[0]
+        if IntOutput < 40 :
+            
         #print(output)
-        lineNotify("CPU usage " + output + " used!")
+            lineNotify("CPU usage " + output + " used!")
 
 
 
