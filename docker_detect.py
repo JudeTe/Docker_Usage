746e6a481fdcef32c798af35da53f5c3471afc7c
Jude <45480093+JudeTe@users.noreply.github.com>
2019-02-01 23:16:06 +0800

    Add files via upload

--- docker_detect.py
+++ docker_detect.py
@@ -3,7 +3,7 @@ from lineNotify import lineNotify
 
 if os.path.exists("docker_usage.txt"):
     with open("docker_usage.txt", "r", encoding="utf-8") as f:
-        output = f.read().split(" ")[2]
+        output = f.read().split(" ")[14]
         IntOutput = output.split(".")[0]
         IntOutput = round(float(IntOutput))
         if IntOutput < 40 :
